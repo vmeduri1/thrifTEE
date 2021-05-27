7480f304b86f38368307aaa08c9ed67c46a87f8c
B Salinas <b.salinas397@gmail.com>
2021-05-27 16:47:09 -0500

    navbar rendering

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -3,8 +3,15 @@ import { NavLink, useHistory } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import { useDispatch, useSelector } from 'react-redux'
 
-
-import { Grid, GridItem } from "@chakra-ui/react"
+import { 
+  Box,
+  Grid, 
+  GridItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator, 
+} from "@chakra-ui/react"
 
 const NavBar = () => {
   const sessionUser = useSelector(state => state.session.user);
@@ -40,33 +47,49 @@ const NavBar = () => {
 
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+    // <nav>
+    //   <ul>
+    //     <li>
+    //       <NavLink to="/" exact={true} activeClassName="active">
+    //         Home
+    //       </NavLink>
+    //     </li>
+    //     <li>
+    //       <NavLink to="/login" exact={true} activeClassName="active">
+    //         Login
+    //       </NavLink>
+    //     </li>
+    //     <li>
+    //       <NavLink to="/sign-up" exact={true} activeClassName="active">
+    //         Sign Up
+    //       </NavLink>
+    //     </li>
+    //     <li>
+    //       <NavLink to="/users" exact={true} activeClassName="active">
+    //         Users
+    //       </NavLink>
+    //     </li>
+    //     <li>
+    //       <LogoutButton />
+    //     </li>
+    //   </ul>
+    // </nav>
+
+    <Grid templateColumns="repeat(12, 1fr)" gap={.5}>
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+      <Box w="100%" h="20" bg="gray.400" />
+    </Grid>
+
   );
 }
 
