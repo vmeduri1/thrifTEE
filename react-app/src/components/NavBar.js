4f08782d3d3a4a40712906351324a8b4d0529ff5
jonathan <jonathanchan.808@gmail.com>
2021-05-27 17:40:25 -0400

    Adding to navbar styling

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,8 +1,44 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import { useDispatch, useSelector } from 'react-redux'
+
+
+import { Grid, GridItem } from "@chakra-ui/react"
 
 const NavBar = () => {
+  const sessionUser = useSelector(state => state.session.user);
+  const events = useSelector(state => state.events);        // pull content out of state
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+
+  // useEffect(() => {
+  //   dispatch(cart())                       // RUN THUNK TO GET
+  // }, [dispatch])
+
+  // if (sessionUser) {            // if session is logged in, then show profile button
+  //   sessionLinks = (
+  //     <ProfileButton user={sessionUser} />                  // using the profile button component here
+  //   );
+  // } else {                  // else, show login and signup buttons instead
+  //   sessionLinks = (
+  //     <>
+  //       <div className='link-btn-container'>
+  //         <div className={'LogInBtn-container'}>
+  //           <LoginFormModal />
+  //         </div>
+
+  //         <div className={'signInBtn-container'}>
+  //           <SignUpFormModal />
+  //         </div>
+  //         {/* <NavLink to="/signup">Sign Up</NavLink> */}
+  //       </div>
+  //     </>
+  //   );
+  // }
+
+
   return (
     <nav>
       <ul>
