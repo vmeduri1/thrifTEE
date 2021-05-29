560f49bee7c7c6e397cfb2732eaef685a2c8d04d
jonathan <jonathanchan.808@gmail.com>
2021-05-29 02:10:01 -0400

    Fixed footer for thank you page

--- react-app/src/components/ThankYou.js
+++ react-app/src/components/ThankYou.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './ThankYou.css';
 // import Footer from '../components/Footer'
 
-
 function ThankYouFunc() {
 
   return (
-    <>
+    // <>
+    <div className='container-for-body'>
       <div className="thank-you-container">
         <img className="thank-you-image" src="https://images-na.ssl-images-amazon.com/images/I/51nxlj1B7zL._AC_SY355_.jpg">
         </img>
         <h2>Order Shipped✓</h2>
       </div>
-    </>
+    </div>
+    // </>
   )
 }
 
