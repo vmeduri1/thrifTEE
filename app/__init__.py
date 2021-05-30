ad605562ab259bc7dc0cb89b5f1b27a7fd6ed409
jonathan <jonathanchan.808@gmail.com>
2021-05-29 23:28:06 -0400

    Starting and working on search function for search bar

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.products_routes import product_routes
 from .api.category_routes import category_routes
+from .api.search_routes import search_routes
 
 from .seeds import seed_commands
 
@@ -35,6 +36,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(product_routes, url_prefix='/api/products')
 app.register_blueprint(category_routes, url_prefix='/api/categories')
+app.register_blueprint(search_routes, url_prefix='/api/search')
 db.init_app(app)
 Migrate(app, db)
 
@@ -46,6 +48,7 @@ CORS(app)
 # Therefore, we need to make sure that in production any
 # request made over http is redirected to https.
 # Well.........
+
 
 @app.before_request
 def https_redirect():
---
+++ app/api/search_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from app.models import db, Product
+
+search_routes = Blueprint('search', __name__, url_prefix='/api/search')
+
+
+@search_routes.route('/<string:query>')
+def get_results(query):
+    results = Post.query.filter(
+        Post.title.ilike(f"%{query}%")
+    ).all()
+
+    return {post.id: post.to_dict() for post in results}
