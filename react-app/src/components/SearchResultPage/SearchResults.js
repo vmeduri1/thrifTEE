8d2c9ae56a0e51df4e1ab83313fb1ba8a0f7862c
jonathan <jonathanchan.808@gmail.com>
2021-07-01 00:20:07 -0400

    working on search function and result page

--- react-app/src/components/SearchResultPage/SearchResults.js
+++ react-app/src/components/SearchResultPage/SearchResults.js
@@ -4,11 +4,13 @@ import Item from '../Item'
 import { searchFunc } from '../../store/search'
 import {
   Flex,
+  Text,
 } from '@chakra-ui/react';
 
 
 const SearchResultsFunc = () => {
   const product_Search = useSelector(state => state.search?.searchResults)
+  // console.log(state.search, '-----search term')
 
 
   const productResults = Object.values(product_Search).map(product => {
@@ -17,9 +19,18 @@ const SearchResultsFunc = () => {
   // console.log(product_Search, '---------SEARCH')
 
   return (
-    <Flex>
-      {productResults}
-    </Flex>
+    <>
+      <Text
+        fontSize='20px'
+        fontWeight='600'
+        mt='50px'
+      >
+        Your search found the following items:
+      </Text>
+      <Flex className='search-container'>
+        {productResults}
+      </Flex>
+    </>
   )
 }
 
--- react-app/src/modals/LoginForm.js
+++ react-app/src/modals/LoginForm.js
@@ -13,7 +13,7 @@ import React, { useState } from "react";
 import * as sessionActions from "../store/session"
 import { login } from '../store/session'
 import { useDispatch, useSelector } from "react-redux";
-import {Redirect} from 'react-router'
+import { Redirect } from 'react-router'
 
 
 
@@ -31,17 +31,17 @@ export function LoginForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setErrors([]);
-    const data= await dispatch(sessionActions.login( email, password ))
+    const data = await dispatch(sessionActions.login(email, password))
 
 
     if (data.errors) setErrors(data.errors)
   };
 
-  const handleDemo= async (e) => {
+  const handleDemo = async (e) => {
     e.preventDefault();
     const email = 'demo@aa.io';
     const password = 'password'
-    const dispatched = await dispatch(sessionActions.login( email, password))
+    const dispatched = await dispatch(sessionActions.login(email, password))
 
 
     if (dispatched.errors) setErrors(dispatch.errors)
@@ -53,46 +53,46 @@ export function LoginForm() {
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <ul style={{marginTop: '.25em', marginBottom: '1.25em'}}>
-        {errors && errors.map((error, idx) => <li key={idx}>{error}</li>)}
-        </ul>
+      <form onSubmit={handleSubmit}>
+        <div style={{ color: 'red', fontWeight: '500' }}>
+          <ul style={{ marginTop: '.25em', marginBottom: '1.25em' }}>
+            {errors && errors.map((error, idx) => <ul key={idx}>{error}</ul>)}
+          </ul>
 
-      </div>
-      <Stack spacing={3}>
-        <FormControl isRequired>
-          <FormLabel>Email</FormLabel>
-          <InputGroup>
-            <Input
-              // ref={initialRef}
-              placeholder="Email"
-              type='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </InputGroup>
-          <FormLabel>Password</FormLabel>
-          <InputGroup>
-            <Input
-              // ref={initialRef}
-              placeholder="Password"
-              type='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </InputGroup>
+        </div>
+        <Stack spacing={3}>
+          <FormControl isRequired>
+            <FormLabel>Email</FormLabel>
+            <InputGroup>
+              <Input
+                // ref={initialRef}
+                placeholder="Email"
+                type='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </InputGroup>
+            <FormLabel>Password</FormLabel>
+            <InputGroup>
+              <Input
+                // ref={initialRef}
+                placeholder="Password"
+                type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </InputGroup>
 
-          <br />
-          <Button type='Submit'>Log in</Button>
+            <br />
+            <Button type='Submit'>Log in</Button>
 
-        </FormControl>
-      </Stack>
-    </form>
+          </FormControl>
+        </Stack>
+      </form>
 
-    <form onSubmit={handleDemo}>
-      <Button type="Submit">Demo User</Button>
-    </form>
+      <form onSubmit={handleDemo}>
+        <Button type="Submit">Demo User</Button>
+      </form>
     </>
   )
 }
