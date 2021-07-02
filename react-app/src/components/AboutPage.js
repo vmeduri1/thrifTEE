2ec9aaf9fc60a24baacee87446f598f849d261b6
jonathan <jonathanchan.808@gmail.com>
2021-07-02 19:13:23 -0400

    Added features, links and redesigned buttons for aboutDev page

--- react-app/src/components/AboutPage.js
+++ react-app/src/components/AboutPage.js
@@ -35,34 +35,39 @@ export default function SocialProfileSimple() {
         textAlign={'center'}>
         <Flex
           // bg='lightblue'
-          w='346.33px'
+          w='347.75px'
           flexDirection='column'
         >
-          <Avatar
-            size={'3xl'}
-            // size={'3xl'}
-            src={
-              'https://avatars.githubusercontent.com/u/60409772?v=4'
-            }
-            alt={'Avatar Alt'}
-            mb={4}
-            pos={'relative'}
-            _after={{
-              content: '""',
-              w: 4,
-              h: 4,
-              border: '2px solid white',
-              pos: 'absolute',
-              bottom: 0,
-              right: 3,
-            }}
-          />
-          <Heading
-            fontSize={'2xl'}
-            fontFamily={'body'}
+          <Link
+            href='https://natoh19.github.io/'
+          // style={{ textDecoration: 'none' }}
           >
-            Natalie Oldroyd
-          </Heading>
+            <Avatar
+              size={'3xl'}
+              // size={'3xl'}
+              src={
+                'https://avatars.githubusercontent.com/u/60409772?v=4'
+              }
+              alt={'Avatar Alt'}
+              mb={4}
+              pos={'relative'}
+              _after={{
+                content: '""',
+                w: 4,
+                h: 4,
+                border: '2px solid white',
+                pos: 'absolute',
+                bottom: 0,
+                right: 3,
+              }}
+            />
+            <Heading
+              fontSize={'2xl'}
+              fontFamily={'body'}
+            >
+              Natalie Oldroyd
+            </Heading>
+          </Link>
         </Flex>
         <Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
           <Badge
@@ -86,32 +91,34 @@ export default function SocialProfileSimple() {
               }}
               w='165.88px'
             >
-              <i className="fab fa-github">
-              </i>
+              <i className="fab fa-github"></i>
             </Button>
           </a>
-          {/* <a href='https://github.com/natoh19'><i className="fab fa-github"></i></a> */}
-          <Button
-            flex={1}
-            fontSize={'2xl'}
-            rounded={'full'}
-            bg={'blue.400'}
-            color={'white'}
-            boxShadow={
-              '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-            }
-            _hover={{
-              bg: 'blue.500',
-            }}
-            _focus={{
-              bg: 'blue.500',
-            }}
-          >
-            <a href='https://www.linkedin.com/in/natalie-oldroyd/'>{<SiLinkedin />}</a>
-          </Button>
+          <a href='https://www.linkedin.com/in/natalie-oldroyd/'>
+            <Button
+              flex={1}
+              fontSize={'2xl'}
+              rounded={'full'}
+              bg={'blue.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'blue.500',
+              }}
+              _focus={{
+                bg: 'blue.500',
+              }}
+              w='165.88px'
+            >
+              {<SiLinkedin />}
+            </Button>
+          </a>
         </Stack>
       </Box >
 
+
       <Box
         margin={'40px'}
         mt={'125px'}
@@ -123,38 +130,41 @@ export default function SocialProfileSimple() {
         rounded={'2xl'}
         p={6}
         textAlign={'center'}>
-        <Avatar
-          size={'3xl'}
-          src={
-            'https://avatars.githubusercontent.com/u/71046664?v=4'
-          }
-          alt={'Avatar Alt'}
-          mb={4}
-          pos={'relative'}
-          _after={{
-            content: '""',
-            w: 4,
-            h: 4,
-            border: '2px solid white',
-            pos: 'absolute',
-            bottom: 0,
-            right: 3,
-          }}
-        />
-        <Heading fontSize={'2xl'} fontFamily={'body'}>
-          Vishaal Meduri
-        </Heading>
-        {/* <Text
-            textAlign={'center'}
-            color={useColorModeValue('gray.700', 'gray.400')}
-            px={3}>
-            Actress, musician, songwriter and artist. PM for work inquires or{' '}
-            <Link href={'#'} color={'blue.400'}>
-              #tag
-            </Link>{' '}
-            me in your posts
-          </Text> */}
-
+        <Flex
+          // bg='lightblue'
+          w='347.75px'
+          flexDirection='column'
+        >
+          <Link
+            href='https://vmeduri1.github.io/'
+          // style={{ textDecoration: 'none' }}
+          >
+            <Avatar
+              size={'3xl'}
+              src={
+                'https://avatars.githubusercontent.com/u/71046664?v=4'
+              }
+              alt={'Avatar Alt'}
+              mb={4}
+              pos={'relative'}
+              _after={{
+                content: '""',
+                w: 4,
+                h: 4,
+                border: '2px solid white',
+                pos: 'absolute',
+                bottom: 0,
+                right: 3,
+              }}
+            />
+            <Heading
+              fontSize={'2xl'}
+              fontFamily={'body'}
+            >
+              Vishaal Meduri
+            </Heading>
+          </Link>
+        </Flex>
         <Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
           <Badge
             px={2}
@@ -166,37 +176,45 @@ export default function SocialProfileSimple() {
             Software Developer
           </Badge>
         </Stack>
-
         <Stack mt={8} direction={'row'} spacing={4}>
-          <Button
-            flex={1}
-            fontSize={'2xl'}
-            rounded={'full'}
-            _focus={{
-              bg: 'gray.200',
-            }}>
-            <a href='https://github.com/vmeduri1'><i className="fab fa-github"></i></a>
-          </Button>
-          <Button
-            flex={1}
-            fontSize={'2xl'}
-            rounded={'full'}
-            bg={'blue.400'}
-            color={'white'}
-            boxShadow={
-              '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-            }
-            _hover={{
-              bg: 'blue.500',
-            }}
-            _focus={{
-              bg: 'blue.500',
-            }}>
-            <a href='https://www.linkedin.com/in/vishaalmeduri/'>{<SiLinkedin />}</a>
-          </Button>
+          <a href='https://github.com/vmeduri1'>
+            <Button
+              flex={1}
+              fontSize={'2xl'}
+              rounded={'full'}
+              _focus={{
+                bg: 'gray.200',
+              }}
+              w='165.88px'
+            >
+              <i className="fab fa-github"></i>
+            </Button>
+          </a>
+          <a href='https://www.linkedin.com/in/vishaalmeduri/'>
+            <Button
+              flex={1}
+              fontSize={'2xl'}
+              rounded={'full'}
+              bg={'blue.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'blue.500',
+              }}
+              _focus={{
+                bg: 'blue.500',
+              }}
+              w='165.88px'
+            >
+              {<SiLinkedin />}
+            </Button>
+          </a>
         </Stack>
       </Box>
 
+
       <Box
         margin={'40px'}
         mt={'125px'}
@@ -208,38 +226,41 @@ export default function SocialProfileSimple() {
         rounded={'2xl'}
         p={6}
         textAlign={'center'}>
-        <Avatar
-          size={'3xl'}
-          src={
-            'https://avatars.githubusercontent.com/u/70030725?v=4'
-          }
-          alt={'Avatar Alt'}
-          mb={4}
-          pos={'relative'}
-          _after={{
-            content: '""',
-            w: 4,
-            h: 4,
-            border: '2px solid white',
-            pos: 'absolute',
-            bottom: 0,
-            right: 3,
-          }}
-        />
-        <Heading fontSize={'2xl'} fontFamily={'body'}>
-          Jonathan Chan
-        </Heading>
-        {/* <Text
-            textAlign={'center'}
-            color={useColorModeValue('gray.700', 'gray.400')}
-            px={3}>
-            Actress, musician, songwriter and artist. PM for work inquires or{' '}
-            <Link href={'#'} color={'blue.400'}>
-              #tag
-            </Link>{' '}
-            me in your posts
-          </Text> */}
-
+        <Flex
+          // bg='lightblue'
+          w='347.75px'
+          flexDirection='column'
+        >
+          <Link
+            href='https://jc-008.github.io/'
+          // style={{ textDecoration: 'none' }}
+          >
+            <Avatar
+              size={'3xl'}
+              src={
+                'https://avatars.githubusercontent.com/u/70030725?v=4'
+              }
+              alt={'Avatar Alt'}
+              mb={4}
+              pos={'relative'}
+              _after={{
+                content: '""',
+                w: 4,
+                h: 4,
+                border: '2px solid white',
+                pos: 'absolute',
+                bottom: 0,
+                right: 3,
+              }}
+            />
+            <Heading
+              fontSize={'2xl'}
+              fontFamily={'body'}
+            >
+              Jonathan Chan
+            </Heading>
+          </Link>
+        </Flex>
         <Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
           <Badge
             px={2}
@@ -251,37 +272,45 @@ export default function SocialProfileSimple() {
             Software Developer
           </Badge>
         </Stack>
-
         <Stack mt={8} direction={'row'} spacing={4}>
-          <Button
-            flex={1}
-            fontSize={'2xl'}
-            rounded={'full'}
-            _focus={{
-              bg: 'gray.200',
-            }}>
-            <a href='https://github.com/Jc-008'><i className="fab fa-github"></i></a>
-          </Button>
-          <Button
-            flex={1}
-            fontSize={'2xl'}
-            rounded={'full'}
-            bg={'blue.400'}
-            color={'white'}
-            boxShadow={
-              '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-            }
-            _hover={{
-              bg: 'blue.500',
-            }}
-            _focus={{
-              bg: 'blue.500',
-            }}>
-            <a href='https://www.linkedin.com/in/jonathan-chan-14438952/'>{<SiLinkedin />}</a>
-          </Button>
+          <a href='https://github.com/Jc-008'>
+            <Button
+              flex={1}
+              fontSize={'2xl'}
+              rounded={'full'}
+              _focus={{
+                bg: 'gray.200',
+              }}
+              w='165.88px'
+            >
+              <i className="fab fa-github"></i>
+            </Button>
+          </a>
+          <a href='https://www.linkedin.com/in/jonathan-chan8/'>
+            <Button
+              flex={1}
+              fontSize={'2xl'}
+              rounded={'full'}
+              bg={'blue.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'blue.500',
+              }}
+              _focus={{
+                bg: 'blue.500',
+              }}
+              w='165.88px'
+            >
+              {<SiLinkedin />}
+            </Button>
+          </a>
         </Stack>
       </Box>
 
+
       <Box
         margin={'40px'}
         mt={'125px'}
@@ -293,38 +322,41 @@ export default function SocialProfileSimple() {
         rounded={'2xl'}
         p={6}
         textAlign={'center'}>
-        <Avatar
-          size={'3xl'}
-          src={
-            'https://avatars.githubusercontent.com/u/31112601?v=4'
-          }
-          alt={'Avatar Alt'}
-          mb={4}
-          pos={'relative'}
-          _after={{
-            content: '""',
-            w: 4,
-            h: 4,
-            border: '2px solid white',
-            pos: 'absolute',
-            bottom: 0,
-            right: 3,
-          }}
-        />
-        <Heading fontSize={'2xl'} fontFamily={'body'}>
-          B Salinas
-        </Heading>
-        {/* <Text
-            textAlign={'center'}
-            color={useColorModeValue('gray.700', 'gray.400')}
-            px={3}>
-            Actress, musician, songwriter and artist. PM for work inquires or{' '}
-            <Link href={'#'} color={'blue.400'}>
-              #tag
-            </Link>{' '}
-            me in your posts
-          </Text> */}
-
+        <Flex
+          // bg='lightblue'
+          w='347.75px'
+          flexDirection='column'
+        >
+          <Link
+            href='https://b-salinas.github.io/'
+          // style={{ textDecoration: 'none' }}
+          >
+            <Avatar
+              size={'3xl'}
+              src={
+                'https://avatars.githubusercontent.com/u/31112601?v=4'
+              }
+              alt={'Avatar Alt'}
+              mb={4}
+              pos={'relative'}
+              _after={{
+                content: '""',
+                w: 4,
+                h: 4,
+                border: '2px solid white',
+                pos: 'absolute',
+                bottom: 0,
+                right: 3,
+              }}
+            />
+            <Heading
+              fontSize={'2xl'}
+              fontFamily={'body'}
+            >
+              B Salinas
+            </Heading>
+          </Link>
+        </Flex>
         <Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
           <Badge
             px={2}
@@ -336,34 +368,41 @@ export default function SocialProfileSimple() {
             Software Developer
           </Badge>
         </Stack>
-
         <Stack mt={8} direction={'row'} spacing={4}>
-          <Button
-            flex={1}
-            fontSize={'2xl'}
-            rounded={'full'}
-            _focus={{
-              bg: 'gray.200',
-            }}>
-            <a href='https://github.com/B-Salinas'><i className="fab fa-github"></i></a>
-          </Button>
-          <Button
-            flex={1}
-            fontSize={'2xl'}
-            rounded={'full'}
-            bg={'blue.400'}
-            color={'white'}
-            boxShadow={
-              '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-            }
-            _hover={{
-              bg: 'blue.500',
-            }}
-            _focus={{
-              bg: 'blue.500',
-            }}>
-            <a href='https://www.linkedin.com/in/b-salinas/'>{<SiLinkedin />}</a>
-          </Button>
+          <a href='https://github.com/B-Salinas'>
+            <Button
+              flex={1}
+              fontSize={'2xl'}
+              rounded={'full'}
+              _focus={{
+                bg: 'gray.200',
+              }}
+              w='165.88px'
+            >
+              <i className="fab fa-github"></i>
+            </Button>
+          </a>
+          <a href='https://www.linkedin.com/in/b-salinas/'>
+            <Button
+              flex={1}
+              fontSize={'2xl'}
+              rounded={'full'}
+              bg={'blue.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'blue.500',
+              }}
+              _focus={{
+                bg: 'blue.500',
+              }}
+              w='165.88px'
+            >
+              {<SiLinkedin />}
+            </Button>
+          </a>
         </Stack>
       </Box>
     </Flex >
