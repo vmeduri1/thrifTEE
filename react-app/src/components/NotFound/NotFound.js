26d0b52fc3c88b0f47133029ca0f590ef259acda
jonathan <jonathanchan.808@gmail.com>
2021-07-08 18:17:12 -0400

    Fixed 404 page

--- react-app/src/components/NotFound/NotFound.js
+++ react-app/src/components/NotFound/NotFound.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './notfound.css'
-import {Button} from "@chakra-ui/react"
-import {useHistory} from 'react-router-dom'
+import { Button } from "@chakra-ui/react"
+import { useHistory } from 'react-router-dom'
 
 export default function NotFound() {
 
     const history = useHistory()
 
-    const handleNotFound= () => {
+    const handleNotFound = () => {
         history.push('/')
     }
     return (
@@ -15,9 +15,9 @@ export default function NotFound() {
 
         <div className="notFound-container">
             <h1 className="notFound-headline">Sorry! The page you're looking for cannot be found.</h1>
-            <div className ="notFound-img"><img src="https://res.cloudinary.com/dse6ebwba/image/upload/v1625359990/404_bim36x.png" role="presentation" alt="not found" className="404-image"/></div>
+            <div className="notFound-img"><img src="https://res.cloudinary.com/dse6ebwba/image/upload/v1625359990/404_bim36x.png" role="presentation" alt="not found" className="404-image" /></div>
 
-            <p className="notFound-text">Try searching or go to our home page to continue shopping.</p>
+            <p className="notFound-text">Try logging in to checkout or go to our home page to continue shopping.</p>
             <div className="btn-notFound"> <Button onClick={handleNotFound} variant="outline" cursor="pointer">Go to thrifTEE Home Page</Button></div>
 
 
--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -4,10 +4,11 @@ import { useSelector } from "react-redux";
 
 const ProtectedRoute = props => {
   const user = useSelector(state => state.session.user)
-  
+
   return (
     <Route {...props}>
-      {(user) ? props.children  : <Redirect to="/login" />}
+      {(user) ? props.children : <Redirect to="/login" />}
+      {/* {(user) ? props.children : <Redirect to="/" />} */}
     </Route>
   )
 };
